2c86cb15baff5405d8521af9095dc8bf192dc256
Thomas Jakway <tjakway@nyu.edu>
2016-01-14 09:22:38 -0800

    yikes

--- src/Jakway/Util/RigidList.hs
+++ src/Jakway/Util/RigidList.hs
@@ -1,17 +1,21 @@
 {-# LANGUAGE TypeSynonymInstances #-}
+import Prelude hiding (Left, Right)
 import Control.Monad
 import Data.Maybe (fromJust)
 
-newtype RigidList a = RigidList [Maybe a]
+data RigidList a = Left [Maybe a] | Right [Maybe a]
+
+unwrapRigidList (Right a) = a
+unwrapRigidList (Left a) = a
 
 instance Monad RigidList where
-        return = return . return
+        return = Right . return . return
 
         -- |This matches what you would expect
         -- because [] :: [Maybe a] is perfectly valid
-        (>>=) [] _ = []
-        (>>=) a f
+        (>>=) (Left x) f = Left x
+        (>>=) (Right y) f
                 -- |Short circuit but don't change the list
                 -- otherwise we might not know there was an error!
-                | a `elem` Nothing = a
-                | otherwise = a >>= (f . fromJust)
+                | y `elem` Nothing = Left y
+                | otherwise = map (f . fromJust) y
